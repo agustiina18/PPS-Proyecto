2bfb848e1065404d2c5a1b65f26cc91e8ae3382a
Agustina Del Castillo <138183022+agustiina18@users.noreply.github.com>
2025-09-16 23:26:57 -0300

    Update app-react.js

--- app-react.js
+++ app-react.js
@@ -1,13 +1,1 @@
-import React from "https://esm.sh/react";
-import ReactDOM from "https://esm.sh/react-dom";
 
-function App() {
-  return (
-    <div>
-      <h1>Bienvenido a tu Agenda</h1>
-      <p>Aquí verás tus turnos y el calendario.</p>
-    </div>
-  );
-}
-
-ReactDOM.render(<App />, document.getElementById("react-app-root"));
